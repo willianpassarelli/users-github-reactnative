ad89d47a68f9eabb6d72ad52484a47ff761ed8cc
willianpassarelli <devpassarelli@gmail.com>
2019-09-15 15:43:00 -0300

    inicializando aplicação

---
+++ src/pages/Main/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = () => {};
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+          />
+          <SubmitButton>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
